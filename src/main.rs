43b340ccaa015b3807cb066cf283af79d282765a
UnknownSuperficialNight <88142731+UnknownSuperficialNight@users.noreply.github.com>
2024-04-08 20:17:58 +1200

    Improved the Rust version, added catch function to reset GPU to auto on exit, and included code comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
-use std::process::{self, Command, Stdio};
+use std::process::{self, exit, Command, Stdio};
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 use termsize as tsize;
-//use std::io;
+
+// Defines interval to refresh
 const REFRESH_TIME: u8 = 5;
-static FAN_AMOUNT: i32 = 1;
+
+#[cfg(feature = "fan_amount_2")]
+const FAN_AMOUNT: u8 = 2;
+
+#[cfg(feature = "fan_amount_3")]
+const FAN_AMOUNT: u8 = 3;
+
+#[cfg(feature = "fan_amount_4")]
+const FAN_AMOUNT: u8 = 4;
+
+// Input your gpu fan amount here
+#[cfg(not(any(
+    feature = "fan_amount_2",
+    feature = "fan_amount_3",
+    feature = "fan_amount_4"
+)))]
+const FAN_AMOUNT: u8 = 1; // Default value when none of the other options are specified
+
+// Input your gpu number here (if you have 1 gpu its normally nought so just leave it)
+const GPU_NUMBER: u8 = 0;
 
 fn check_sudo() {
     if let Ok(user) = std::env::var("USER") {
@@ -36,7 +57,7 @@ fn temp_loop() -> i32 {
     let mut speed_output = 0;
     let temp = temp_func();
     let target: i32 = temp.trim().parse().expect("Wanted a number");
-    let mut speed_output_diff = 999; // some high value
+    let mut speed_output_diff = 999;
     let speed: [i32; 10] = [10, 20, 30, 40, 59, 70, 80, 90, 95, 100];
     for x in speed {
         let diff = (x - target).abs();
@@ -61,8 +82,47 @@ fn temp_capture() -> u8 {
     return temp_capture2.try_into().unwrap();
 }
 
+fn cleanup() {
+    //
+    //
+    //
+    //
+    //
+    //                              ┌─────────────────┐
+    //                              │ Set Gpu to auto │
+    //                              └─────────────────┘
+    Command::new("nvidia-settings")
+        .arg("-a")
+        .arg(&format!("[gpu:{}]/GPUFanControlState=0", GPU_NUMBER))
+        .output()
+        .expect("nvidia-settings command failed to execute");
+}
+
 fn main() {
     check_sudo();
+
+    // Create a channel for communication between threads
+    let (tx, rx) = mpsc::channel();
+
+    // Set up the Ctrl+C handler
+    ctrlc::set_handler(move || {
+        // Send a signal on the channel when Ctrl+C is pressed
+        tx.send(()).expect("Could not send signal on channel.");
+    })
+    .expect("Error setting Ctrl+C handler");
+
+    // Spawn a new thread to execute the cleanup function
+    thread::spawn(move || {
+        // Wait for the signal from the main thread
+        rx.recv().expect("Could not receive from channel.");
+
+        // Execute the cleanup function
+        cleanup();
+
+        // Exit the program
+        exit(0);
+    });
+
     let mut temp_capture_call = temp_capture();
     loop {
         let temp_capture = temp_capture();
@@ -72,7 +132,7 @@ fn main() {
 
         let gpu_temp_str = format!("gpu_temp {}", temp);
         let fan_speed_output_str = format!("fan_speed_output {}", speed_output);
-        // let temp_capture_str = format!("temp_capture {}", temp_capture_call);
+        let skip = format!("Skip command as speed has not changed");
 
         // Get the terminal size
         if let Some(size) = tsize::get() {
@@ -81,7 +141,7 @@ fn main() {
             // Calculate the center position
             let temp_center = (width - gpu_temp_str.len()) / 2;
             let speed_output_center = (width - fan_speed_output_str.len()) / 2;
-            // let temp_capture_center = (width - temp_capture_str.len()) / 2;
+            let skip_center = (width - skip.len()) / 2;
 
             // Print the formatted output at the calculated center positions
             println!(
@@ -94,14 +154,9 @@ fn main() {
                 fan_speed_output_str,
                 width = speed_output_center + fan_speed_output_str.len()
             );
-            // println!(
-            //     "{: >width$}",
-            //     temp_capture_str,
-            //     width = temp_capture_center + temp_capture_str.len()
-            // );
+            println!("{: >width$}", skip, width = skip_center + skip.len());
 
             if speed_output == temp_capture_call.into() {
-                println!("Skip command as speed has not changed");
             } else {
                 for faninc in 0..FAN_AMOUNT {
                     Command::new("nvidia-settings")
